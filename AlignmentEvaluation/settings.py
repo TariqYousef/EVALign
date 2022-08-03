5aebf7fc19117a93ea9b0615dfccd11b3dcc07b6
TariqYousef <info@tariq-yousef.com>
2022-08-03 10:24:46 +0200

    cleaning

--- AlignmentEvaluation/settings.py
+++ AlignmentEvaluation/settings.py
@@ -11,21 +11,22 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n70zvhy5pcx!#(_@=a8sq9lix2)yr1kpi=sg^5i80t9s^%4&a7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '[::1]']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "*").split(" ")
 
 
 # Application definition
@@ -81,17 +82,13 @@ WSGI_APPLICATION = 'AlignmentEvaluation.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backends.postgresql_psycopg2'),
-        'NAME': os.environ.get("SQL_DATABASE", 'evalign'),
-        'USER': os.environ.get("SQL_USER", 'postgres'),
-        'PASSWORD': os.environ.get("SQL_PASSWORD", 'postgres2020'),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+        'NAME': os.environ.get("SQL_DATABASE", ''),
+        'USER': os.environ.get("SQL_USER", ''),
+        'PASSWORD': os.environ.get("SQL_PASSWORD", ''),
+        "HOST": os.environ.get("SQL_HOST"),
+        "PORT": os.environ.get("SQL_PORT"),
     }
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ### EVALign: Visual Evaluation of Translation Alignment Models
 
-Installation instructions and documentation will be available soon.
+**EVALign** is a framework for quantitative and qualitative evaluation of automatic translation alignment models. **EVALign** offers several visualization views enabling developers to visualize their models' predictions and compare the performance of their models with other baseline and state-of-the-art models. 
+Via different search and filter functions, developers can also inspect the frequent alignment errors and their positions. **EVALign** hosts nine gold standard datasets and the predictions of multiple alignment models. The tool is extendable, and adding additional datasets and models is straightforward. 
+
+
+_Installation instructions and documentation will be available soon._
+
